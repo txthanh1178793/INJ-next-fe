17a9a91e170660b6de3244ddd7d31dc1098eed5c
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-04-25 18:31:55 +0700

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,10 +27,10 @@ function Home({ }: Props) {
             </div>
             <div className="ggzm1z0 _1nzuaz710 _1nzuaz72">
               <div className="sc-3a5c8d1f-1 sc-32d5f017-0 lbEZGW fOPopv">
-                <Link href="/" className="sc-bcdfb9f2-0 fQQnxa sc-437c0afe-1 bTxNJR">Predict</Link>
+                {/* <Link href="/" className="sc-bcdfb9f2-0 fQQnxa sc-437c0afe-1 bTxNJR">Predict</Link>
                 <Link href="/" className="sc-bcdfb9f2-0 fQQnxa sc-437c0afe-1 bTxNJR">Lottery</Link>
                 <Link href="/" className="sc-bcdfb9f2-0 fQQnxa sc-437c0afe-1 bTxNJR">About</Link>
-                <Link href="/" className="sc-bcdfb9f2-0 fQQnxa sc-437c0afe-1 bTxNJR">Community</Link>
+                <Link href="/" className="sc-bcdfb9f2-0 fQQnxa sc-437c0afe-1 bTxNJR">Community</Link> */}
               </div>
             </div>
           </div>
