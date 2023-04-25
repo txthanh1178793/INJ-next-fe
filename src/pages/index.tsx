2297f675cc257fa52e85cef017f33c0f355ec904
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-04-25 18:33:58 +0700

    index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useCounterStore } from "@/context/CounterContextProvider";
 import React, { useEffect, useState } from "react";
-import Link from 'next/link';
+
 type Props = {};
 
 function Home({ }: Props) {
@@ -15,42 +15,38 @@ function Home({ }: Props) {
     setContractCounter(inputCount);
   }
   return (
-    <div>
-      <body>
-        <nav className="sc-ff5a39ab-1 cZeLOv">
-          <div className="sc-3a5c8d1f-1 sc-32d5f017-0 glwDJB fOPopv">
-            <div className="sc-3a5c8d1f-1 sc-32d5f017-0 glwDJB chfQFH">
-              <object type="image/svg+xml" data="DOM_Predict.svg"></object>
-              <svg viewBox="0 0 1281 199" className="sc-231a1e38-0 dPwWVs desktop-icon" color="text" width="200px">
-                <use xlinkHref="#icomoon-ignore" />
-              </svg>
-            </div>
-            <div className="ggzm1z0 _1nzuaz710 _1nzuaz72">
-              <div className="sc-3a5c8d1f-1 sc-32d5f017-0 lbEZGW fOPopv">
-                {/* <Link href="/" className="sc-bcdfb9f2-0 fQQnxa sc-437c0afe-1 bTxNJR">Predict</Link>
-                <Link href="/" className="sc-bcdfb9f2-0 fQQnxa sc-437c0afe-1 bTxNJR">Lottery</Link>
-                <Link href="/" className="sc-bcdfb9f2-0 fQQnxa sc-437c0afe-1 bTxNJR">About</Link>
-                <Link href="/" className="sc-bcdfb9f2-0 fQQnxa sc-437c0afe-1 bTxNJR">Community</Link> */}
-              </div>
-            </div>
-          </div>
-
-          <div className="sc-3a5c8d1f-1 sc-32d5f017-0 qopKT chfQFH">
-            <button className="sc-56a7a3a9-0 cyLbdv" >
-              <div>Connect Wallet</div>
+    <div className='flex justify-center pt-20'>
+      <div className='bg-white rounded-lg p-5 text-center'>
+        <div>
+          <h1>The Count is</h1>
+          <p>{isLoading ? "loading..." : count}</p>
+        </div>
+        <div>
+          <button
+            onClick={incrementCount}
+            className='btn w-full'
+            disabled={isLoading}
+          >
+            +
+          </button>
+          <div className='py-2 flex gap-2'>
+            <input
+              type='number'
+              value={inputCount}
+              step={1}
+              onChange={(e) => setInputCount(e.target.value)}
+              className='border rounded-lg p-2'
+            />
+            <button
+              onClick={handleSetCount}
+              className='btn'
+              disabled={isLoading}
+            >
+              Set Count
             </button>
           </div>
-        </nav>
-
-        <div className="sc-3a5c8d1f-1 sc-ff5a39ab-4 iuuTdc ehOcez bPOyEc">
-          <div className="sc-667a20da-4 eOKfsA">
-            <div className="sc-667a20da-1 iWIALT">
-              <div className="sc-667a20da-5 dGJnQD">
-              </div>
-            </div>
-          </div>
         </div>
-      </body >
+      </div>
     </div>
   );
 }
