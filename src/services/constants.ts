65ec3730d1e9cce8634a4ae48202682b008a4392
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-04-25 18:05:19 +0700

    init

---
+++ src/services/constants.ts
@@ -0,0 +1,34 @@
+import { Network } from "@injectivelabs/networks";
+import { ChainId, EthereumChainId } from "@injectivelabs/ts-types";
+
+export const isProduction = false;
+
+export const IS_DEVELOPMENT: boolean = true;
+export const IS_PRODUCTION: boolean = false;
+
+const env = {
+  NEXT_ALCHEMY_GOERLI_KEY: "qMVuB_50VSArugxMaOG4RsUK7hiQI1Ip",
+  NEXT_NETWORK: Network.Testnet,
+  NEXT_ETHEREUM_CHAIN_ID: EthereumChainId.Goerli,
+  NEXT_CHAIN_ID: ChainId.Testnet,
+};
+
+export const ALCHEMY_GOERLI_KEY = "qMVuB_50VSArugxMaOG4RsUK7hiQI1Ip";
+
+export const alchemyRpcEndpoint = `https://eth-mainnet.g.alchemy.com/v2/qMVuB_50VSArugxMaOG4RsUK7hiQI1Ip`;
+export const alchemyWsRpcEndpoint = `wss://eth-mainnet.g.alchemy.com/v2/qMVuB_50VSArugxMaOG4RsUK7hiQI1Ip`;
+
+export const ETHEREUM_CHAIN_ID = (env.NEXT_ETHEREUM_CHAIN_ID ||
+  EthereumChainId.Goerli) as EthereumChainId;
+export const CHAIN_ID = (env.NEXT_CHAIN_ID || ChainId.Testnet) as ChainId;
+
+export const NETWORK: Network =
+  (env.NEXT_NETWORK as Network) || Network.Testnet;
+
+export const IS_TESTNET: Boolean = [
+  Network.Testnet,
+  Network.TestnetK8s,
+].includes(NETWORK);
+
+export const COUNTER_CONTRACT_ADDRESS =
+  "inj1t8rhq5vcxqgw68ldg0k2mjxjvzshuah6tnugvy";
