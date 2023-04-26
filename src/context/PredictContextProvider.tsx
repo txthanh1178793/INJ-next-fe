7fc564f768e6f6ff22d90176334fa95ae7bd60d2
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-04-26 23:11:55 +0700

    test return raw response

--- src/context/PredictContextProvider.tsx
+++ src/context/PredictContextProvider.tsx
@@ -63,16 +63,16 @@ const CounterContextProvider = (props: Props) => {
 
     async function fetchCount() {
         try {
-            const response = (await chainGrpcWasmApi.fetchSmartContractState(
+            const response = await chainGrpcWasmApi.fetchSmartContractState(
                 PREDICT_CONTRACT_ADDRESS,
                 toBase64({ current_info: { addr: "inj1lxz8ty4rdulcux5knduj686097gawxwmwe8w5w" } })
-            )) as { data: string };
+            );
 
             // const { count } = fromBase64(response.data) as { count: number };
             // setCount(count);
 
-            const count = response.data;
-            console.log(count);
+            // const count = response.data;
+            console.log(response);
         } catch (e) {
             alert((e as any).message);
         }
