2c8160f6662f7ed80292f4aa249db46eecb2f783
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-05-02 00:54:32 +0700

    add position

--- src/components/CurrentBet.tsx
+++ src/components/CurrentBet.tsx
@@ -88,11 +88,11 @@ const CurrentBet = (props: Props) => {
                         </tr>
                         <tr>
                             <td className="prize">Up Postion</td>
-                            <td>${(parseInt(BigInt(info.upPosition as string) / BigInt('100000000000000')) / 10000).toString()} INJ</td>
+                            <td>${(parseInt((BigInt(info.upPosition) / BigInt('100000000000000')).toString()) / 10000).toString()} INJ</td>
                         </tr>
                         <tr>
                             <td className="prize">Down Postion</td>
-                            <td>${(parseInt(BigInt(info.downPosition as string) / BigInt('100000000000000')) / 10000).toString()} INJ</td>
+                            <td>${(parseInt((BigInt(info.downPosition) / BigInt('100000000000000')).toString()) / 10000).toString()} INJ</td>
                         </tr>
                     </table>
                 </div>
