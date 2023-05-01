a0eac978ec9a8d9e9e048473975ecc1cc659d1ef
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-05-02 04:10:50 +0700

    fix button

--- src/components/CurrentBet.tsx
+++ src/components/CurrentBet.tsx
@@ -105,14 +105,14 @@ const CurrentBet = (props: Props) => {
                 </div>
                 <input type="number" className="input" value={inputValue} onChange={(e) => handleChange(e)} />
                 <div className="--button-container ---a">
-                    <button onClick={handleUpBet} disabled={info.status != "1"} className="button ---a"></button>
+                    <button onClick={handleUpBet} disabled={info.status != "1"} className="button ---a">UP</button>
                     {/* <p className="--button-text">UP</p> */}
                     <object className="--center-vertical" data="up.svg" width="50" height="50"> </object>
 
                 </div>
                 <div className="--button-container ---b">
-                    <button onClick={handleDownBet} disabled={info.status != "1"} className="button ---b"></button>
-                    {/* <object className="--center-vertical" data="down.svg" width="40" height="40"> </object> */}
+                    <button onClick={handleDownBet} disabled={info.status != "1"} className="button ---b">DOWN</button>
+                    <object className="--center-vertical" data="down.svg" width="40" height="40"> </object>
                     {/* <p className="--button-text">DOWN</p> */}
                 </div>
             </div>
