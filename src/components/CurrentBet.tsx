c19b92d435f4dc86dba6eb56376887c67f96b983
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-05-01 19:20:06 +0700

    add current bet html

---
+++ src/components/CurrentBet.tsx
@@ -0,0 +1,47 @@
+import { useWalletStore } from "@/context/WalletContextProvider";
+import React from "react";
+
+type Props = {};
+const CurrentBet = (props: Props) => {
+    return (
+        <div className="--container-wrapper">
+            <div className="--container-inner">
+                <p className="order">#1420</p>
+                <div>
+                    <h2 onload="startTime()"></h2>
+                    <div id="txt"></div>
+                </div>
+                <div className="line"></div>
+                <div className="price-tag">
+                    <p>INJ Price</p>
+                    <p className="price">$7.863</p>
+                    <table className="info">
+                        <tr>
+                            <th className="price-start">Start Price</th>
+                            <th>$7.862</th>
+                        </tr>
+                        <tr>
+                            <td className="prize">Total Prize</td>
+                            <td>1100.2 INJ</td>
+                        </tr>
+                    </table>
+                </div>
+                <input type="number" className="input" />
+                <div className="--button-container ---a">
+                    <p className="--button-text">UP</p>
+                    <object className="--center-vertical" data="up.svg" width="50" height="50"> </object>
+
+
+
+                </div>
+                <div className="--button-container ---b">
+                    <object className="--center-vertical" data="down.svg" width="40" height="40"> </object>
+                    <p className="--button-text">DOWN</p>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default CurrentBet;
