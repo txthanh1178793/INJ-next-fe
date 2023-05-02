02c2029bc29d679451065e26490f66f9f73e6bfb
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-05-02 14:35:02 +0700

    add bet info data

--- src/components/CurrentBet.tsx
+++ src/components/CurrentBet.tsx
@@ -144,7 +144,7 @@ const CurrentBet = (props: Props) => {
                         <tr>
                             <td className="--bet-info-data">Total Prize</td>
                             <td className="--bet-info-data">
-                                {parseInt((bigInt(betInfo.totalPrize) / bigInt("10000000000000")) as string) / 100000} $INJ
+                                {parseInt((bigInt(betInfo.totalPrize) / bigInt("10000000000000")).toString) / 100000} $INJ
                             </td>
                         </tr>
                     </table>
