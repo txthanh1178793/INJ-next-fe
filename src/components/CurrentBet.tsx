2b245775dc7af456ceb77e7a9008ae8c50d8a491
txthanh1178793 <truongxuanthanh1178793@gmail.com>
2023-05-02 13:01:26 +0700

    change bet info tag position

--- src/components/CurrentBet.tsx
+++ src/components/CurrentBet.tsx
@@ -78,50 +78,50 @@ const CurrentBet = (props: Props) => {
     };
     // onMouseOver={handleQueryCurrentInfo} 
     return (
-        <div className="--container-wrapper" >
-            <div className="--container-inner">
-                <p className="order">
-                    #{info.id} {parseInt(info.endTime) == 0 ? "Pending" : (parseInt(info.endTime) < parseInt(info.timeStamp) ? "Watting for Result" : ("Betting End in " + (parseInt(info.endTime) - parseInt(info.timeStamp)).toString() + ' s'))}
-                </p>
-                <div className="line"></div>
-                <div className="price-tag">
-                    <p>INJ Price</p>
-                    <p className={parseFloat(info.binancePrice) >= parseFloat(info.startPrice) ? "price-up" : "price-down"} >${info.binancePrice}</p>
-                    <table className="info">
-                        <tr>
-                            <th className="price-start">Start Price</th>
-                            <th>${info.startPrice}</th>
-                        </tr>
-                        <tr>
-                            <td className="prize">Total Prize</td>
-                            <td>{(parseFloat(((BigInt(info.totalUp) + BigInt(info.totalDown)) / BigInt("100000000000000")).toString()) / 10000).toString()} $INJ</td>
-                        </tr>
-                        <tr>
-                            <td className="prize">Up Postion</td>
-                            <td>{(parseFloat((BigInt(info.upPosition) / BigInt('10000000000000')).toString()) / 100000).toString()} $INJ</td>
-                        </tr>
-                        <tr>
-                            <td className="prize">Down Postion</td>
-                            <td>{(parseFloat((BigInt(info.downPosition) / BigInt('10000000000000')).toString()) / 100000).toString()} $INJ</td>
-                        </tr>
-                    </table>
-                </div>
-                <input type="number" className="input" value={inputValue} onChange={(e) => handleChange(e)} />
-                <div className="--button-container ---a">
-                    <button onClick={handleUpBet} disabled={info.status != "1"} className="button ---a">UP</button>
-                    {/* <p className="--button-text">UP</p> */}
-                    <object className="--center-vertical" data="up.svg" width="50" height="50"> </object>
+        <div>
+            <div className="--container-wrapper" >
+                <div className="--container-inner">
+                    <p className="order">
+                        #{info.id} {parseInt(info.endTime) == 0 ? "Pending" : (parseInt(info.endTime) < parseInt(info.timeStamp) ? "Watting for Result" : ("Betting End in " + (parseInt(info.endTime) - parseInt(info.timeStamp)).toString() + ' s'))}
+                    </p>
+                    <div className="line"></div>
+                    <div className="price-tag">
+                        <p>INJ Price</p>
+                        <p className={parseFloat(info.binancePrice) >= parseFloat(info.startPrice) ? "price-up" : "price-down"} >${info.binancePrice}</p>
+                        <table className="info">
+                            <tr>
+                                <th className="price-start">Start Price</th>
+                                <th>${info.startPrice}</th>
+                            </tr>
+                            <tr>
+                                <td className="prize">Total Prize</td>
+                                <td>{(parseFloat(((BigInt(info.totalUp) + BigInt(info.totalDown)) / BigInt("100000000000000")).toString()) / 10000).toString()} $INJ</td>
+                            </tr>
+                            <tr>
+                                <td className="prize">Up Postion</td>
+                                <td>{(parseFloat((BigInt(info.upPosition) / BigInt('10000000000000')).toString()) / 100000).toString()} $INJ</td>
+                            </tr>
+                            <tr>
+                                <td className="prize">Down Postion</td>
+                                <td>{(parseFloat((BigInt(info.downPosition) / BigInt('10000000000000')).toString()) / 100000).toString()} $INJ</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <input type="number" className="input" value={inputValue} onChange={(e) => handleChange(e)} />
+                    <div className="--button-container ---a">
+                        <button onClick={handleUpBet} disabled={info.status != "1"} className="button ---a">UP</button>
+                        {/* <p className="--button-text">UP</p> */}
+                        <object className="--center-vertical" data="up.svg" width="50" height="50"> </object>
 
+                    </div>
+                    <div className="--button-container ---b">
+                        <button onClick={handleDownBet} disabled={info.status != "1"} className="button ---b">DOWN</button>
+                        <object className="--center-vertical" data="down.svg" width="40" height="40"> </object>
+                        {/* <p className="--button-text">DOWN</p> */}
+                    </div>
                 </div>
-                <div className="--button-container ---b">
-                    <button onClick={handleDownBet} disabled={info.status != "1"} className="button ---b">DOWN</button>
-                    <object className="--center-vertical" data="down.svg" width="40" height="40"> </object>
-                    {/* <p className="--button-text">DOWN</p> */}
-                </div>
-            </div>
-
-
-            <div className="-bet--info">
+            </div >
+            <div className="--container-wrapper -bet--info">
                 <div className="--step">
                     <div className="--bet-info-id">#120</div>
                     <a href="#" className="previous round">&#8249;</a>
@@ -151,7 +151,8 @@ const CurrentBet = (props: Props) => {
                     <button className="--check-and-claim"></button>
                 </div>
             </div>
-        </div >
+        </div>
+
     );
 };
 
